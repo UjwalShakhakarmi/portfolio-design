d2769681a254b7dddf2bec512e622bfabde0c581
ujwaldai <ujwalshakha@gmail.com>
2022-11-20 11:02:42 +0545

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+
+    function change()
+    {
+        document.getElementById("reds").style.color="rgba(116, 29, 29, 1)";
+        document.getElementById("blacks").style.color="rgb(0.16, 0.16, 0.16)";
+        document.getElementById("blacks").style.fontSize="15px";
+        document.getElementById("reds").style.fontSize="15px";
+
+    }
+    function unchange()
+    {
+        document.getElementById("reds").style.color="white";
+        document.getElementById("blacks").style.color="white";
+        document.getElementById("blacks").style.fontSize="14px";
+
+
+    }
+
